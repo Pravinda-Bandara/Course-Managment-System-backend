aec8b75da53c3df6ee70690ecb9b897b4915c33b
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-06-12 01:48:23 +0530

    add .env and routs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import courseRoutes from "./routes/courseRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import enrollmentRoutes from "./routes/enrollmentRoutes.js";
 
 const app = express();
 dotenv.config()
@@ -25,6 +28,10 @@ app.use(express.urlencoded({ extended: true }));
 /*app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/contacts', contactRoutes);*/
 
+
+app.use('/api/courses', courseRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/enrollments',enrollmentRoutes)
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT,()=>{
