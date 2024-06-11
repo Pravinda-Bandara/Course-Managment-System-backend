e5cc49b7a113ce8f000b008c08db4de8106b4b0d
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-06-12 02:14:22 +0530

    update UserRouter

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,7 @@ import User from '../models/user.js';
 const router = express.Router();
 
 // Get user by ID
-router.get(
+/*router.get(
     '/:id',
     asyncHandler(async (req, res) => {
         const user = await User.findById(req.params.id);
@@ -17,16 +17,27 @@ router.get(
             res.status(404).send({ message: 'User Not Found' });
         }
     })
-);
+);*/
 
-// Get all users (admin only)
+// Get all admins (admin only)
 router.get(
-    '/',
+    '/admins',
     isAuth,
     isAdmin,
     asyncHandler(async (req, res) => {
-        const users = await User.find({});
-        res.send(users);
+        const admins = await User.find({ role: 'admin' });
+        res.send(admins);
+    })
+);
+
+// Get all students (admin only)
+router.get(
+    '/students',
+    isAuth,
+    isAdmin,
+    asyncHandler(async (req, res) => {
+        const students = await User.find({ role: 'student' });
+        res.send(students);
     })
 );
 
@@ -56,7 +67,7 @@ router.post(
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 8)
+            password: bcrypt.hashSync(req.body.password, 8),
         });
 
         res.send({
