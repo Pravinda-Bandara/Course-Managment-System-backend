9face7259d35df494bbc5abbc7e0c75d8497ef74
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-12-21 20:28:30 +0530

    refactor code with new resposnse structure

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,24 +1,21 @@
-
-
 import asyncHandler from "express-async-handler";
 import Course from "../models/course.js";
 import Enrollment from "../models/enrollment.js";
+import { ApiResponse } from '../utils.js';
 
 export const getCourses = asyncHandler(async (req, res) => {
     const courses = await Course.find();
-    res.json(courses);
+    res.json(ApiResponse({ data: courses }));
 });
-
 
 export const getCourseById = asyncHandler(async (req, res) => {
     const course = await Course.findById(req.params.id);
     if (course) {
-        res.json(course);
+        res.json(ApiResponse({ data: course }));
     } else {
-        res.status(404).json({ message: 'Course not found' });
+        res.status(404).json(ApiResponse({ success: false, error: 'Course not found' }));
     }
 });
-
 
 export const createCourse = asyncHandler(async (req, res) => {
     const { title, description, duration, instructor, instructor_num } = req.body;
@@ -30,9 +27,8 @@ export const createCourse = asyncHandler(async (req, res) => {
         instructor_num
     });
     const createdCourse = await course.save();
-    res.status(201).json(createdCourse);
+    res.status(201).json(ApiResponse({ data: createdCourse }));
 });
-
 
 export const updateCourse = asyncHandler(async (req, res) => {
     const { title, description, duration, instructor, instructor_num } = req.body;
@@ -42,22 +38,21 @@ export const updateCourse = asyncHandler(async (req, res) => {
         course.description = description;
         course.duration = duration;
         course.instructor = instructor;
-        course. instructor_num =  instructor_num;
+        course.instructor_num = instructor_num;
         const updatedCourse = await course.save();
-        res.json(updatedCourse);
+        res.json(ApiResponse({ data: updatedCourse }));
     } else {
-        res.status(404).json({ message: 'Course not found' });
+        res.status(404).json(ApiResponse({ success: false, error: 'Course not found' }));
     }
 });
-
 
 export const deleteCourse = asyncHandler(async (req, res) => {
     const course = await Course.findById(req.params.id);
     if (course) {
         await course.deleteOne();
         await Enrollment.deleteMany({ courseId: req.params.id });
-        res.json({ message: 'Course removed' });
+        res.json(ApiResponse({ data: { message: 'Course removed' } }));
     } else {
-        res.status(404).json({ message: 'Course not found' });
+        res.status(404).json(ApiResponse({ success: false, error: 'Course not found' }));
     }
 });
